4925e95f00d0a2bcfb5833963ebe41d716f6a980
Kathleen Nicholson <knichol8@nd.edu>
2017-11-03 15:12:53 -0400

    Took a stab at Q1.2/Q1.3. Going to ask Meredith what we should be expecting, as these graphs required less time than the 600 used in class/on Q1.1.

--- exercise10q1.py
+++ exercise10q1.py
@@ -34,6 +34,7 @@ def part1():
         params = (r, K)
         sim = oi(func=ddSim, y0=N0, t=times,args=params)
         QIA.iloc[:, i] = sim[:, 0]
+
     
     # plot data
     
@@ -45,7 +46,33 @@ def part2():
     N0 = 1 (these were somewhat arbitrarily chosen to make an 
     informative figure).
     """
-    pass
+def ddSim (y,t,r,K):
+    N=y[0]
+    dNdt=r*(1-N/K)*N
+    return [dNdt]
+# define initial values (these don't change), state variables, time steps, params (that aren't changing)
+# ex. define initial val (N), time steps, param(k)
+r=0.2
+N0=[1]
+times=range(0,2)
+# list of r-values
+Ks=[10,50,100]
+# make Df to store results. Needs column for time and each r. 
+Q1B=pandas.DataFrame({"times":times,"K1":0,"K2":0,"K3":0})
+# Write a for loop to evaluate model at each r. 
+for i in range(0,len(Ks)):
+    params=(Ks[i],r)
+    sim=spint.odeint(func=ddSim,y0=N0,t=times,args=params)
+    Q1B.iloc[:,i]=sim[:,0]
+print ggplot(aes(x='times'), data=Q1B)+\
+    xlab("time")+ylab("capacity") +\
+    geom_line(aes(y='K1'), color='blue') +\
+    geom_line(aes(y='K2'), color='red') +\
+    geom_line(aes(y='K3'), color='green') 
+
+
+
+
 
 def part3():
     """
@@ -54,7 +81,30 @@ def part3():
     initial population size (N0 = 1, 50, and 100). Use a r = 0.1
     and K = 50 for these simulations.
     """
-    pass
+def ddSim (y,t,r,K):
+    N=y[0]
+    dNdt=r*(1-N/K)*N
+    return [dNdt]
+# define initial values (these don't change), state variables, time steps, params (that aren't changing)
+# ex. define initial val (N), time steps, param(k)
+r=0.1
+K=[50]
+times=range(0,3)
+# list of r-values
+Ns=[1,50,100]
+# make Df to store results. Needs column for time and each r. 
+Q1C=pandas.DataFrame({"times":times,"N1":0,"N2":0,"N3":0})
+# Write a for loop to evaluate model at each r. 
+for i in range(0,len(Ns)):
+    params=(Ns[i],r)
+    sim=spint.odeint(func=ddSim,y0=N0,t=times,args=params)
+    Q1C.iloc[:,i]=sim[:,0]
+print ggplot(aes(x='times'), data=Q1C)+\
+    xlab("time")+ylab("population size") +\
+    geom_line(aes(y='N1'), color='blue') +\
+    geom_line(aes(y='N2'), color='yellow') +\
+    geom_line(aes(y='N3'), color='green') 
+
 
 if __name__ == '__main__':
     pass
