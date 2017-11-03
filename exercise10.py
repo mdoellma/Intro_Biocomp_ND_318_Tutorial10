ae49cce65afcc3105fdffb2dbc987196662e5f61
BaileeEgan <began3@nd.edu>
2017-11-03 10:35:52 -0400

    Created initial exercise file

---
+++ exercise10.py
@@ -0,0 +1,15 @@
+import pandas
+import scipy
+import scipy.integrate as spint
+from plotnine import *
+
+### --- Begin challenge 1 ------------------------------------
+
+
+
+### --- End Challenge 1---------------------------------------
+### --- Begin challenge 2-------------------------------------
+
+
+
+### --- End Challenge 2---------------------------------------
