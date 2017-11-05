5b16eb8d82444bab6ac1585fa75b50be370679d7
Kathleen Nicholson <knichol8@nd.edu>
2017-11-05 18:53:23 -0500

    Added graph for Q1.1. Fixed minor details on code comments.

--- exercise10q1.py
+++ exercise10q1.py
@@ -4,7 +4,7 @@ Authors: Grant Keller and Kathleen Nicholson
 
 ## Module description
 """
-<<<<<<< HEAD
+
 
 import pandas
 from scipy.integrate import odeint as oi
@@ -24,16 +24,21 @@ def ddSim(y, t0, r, K):
 def part1():
     """
     """
-    times = range(600)
-    N0, K = 10, 100
-    QIA = pandas.DataFrame({"times":times,
-                            "r1":0, "r2":0,
-                            "r3":0, "r4":0, "r5":0})
-    rs = [-0.1, 0.1, 0.4, 0.8, 1.0]
-    for i, r in enumerate(rs):
-        params = (r, K)
-        sim = oi(func=ddSim, y0=N0, t=times,args=params)
-        QIA.iloc[:, i] = sim[:, 0]
+times = range(600)
+N0, K = 10, 100
+QIA = pandas.DataFrame({"times":times,"r1":0, "r2":0,"r3":0, "r4":0, "r5":0})
+rs = [-0.1, 0.1, 0.4, 0.8, 1.0]
+for i, r in enumerate(rs):
+    params = (r, K)
+    sim = spint.odeint(func=ddSim, y0=N0, t=times,args=params)
+    QIA.iloc[:, i] = sim[:, 0]
+print ggplot(aes(x='times'), data=QIA) +\
+    xlab("time")+ylab("capacity") +\
+    geom_line(aes(y='r1'), color='blue') +\
+    geom_line(aes(y='r2'), color='red') +\
+    geom_line(aes(y='r3'), color='green') +\
+    geom_line(aes(y='r4'), color='yellow') +\
+    geom_line(aes(y='r5'), color='orange')
 
     
     # plot data
@@ -54,17 +59,17 @@ def ddSim (y,t,r,K):
 # ex. define initial val (N), time steps, param(k)
 r=0.2
 N0=[1]
-times=range(0,2)
-# list of r-values
+times=range(0,500)
+# list of K-values
 Ks=[10,50,100]
-# make Df to store results. Needs column for time and each r. 
+# make Df to store results. Needs column for time and each K. 
 Q1B=pandas.DataFrame({"times":times,"K1":0,"K2":0,"K3":0})
-# Write a for loop to evaluate model at each r. 
+# Write a for loop to evaluate model at each K. 
 for i in range(0,len(Ks)):
     params=(Ks[i],r)
     sim=spint.odeint(func=ddSim,y0=N0,t=times,args=params)
     Q1B.iloc[:,i]=sim[:,0]
-print ggplot(aes(x='times'), data=Q1B)+\
+print ggplot(aes(x='times'), data=Q1B) +\
     xlab("time")+ylab("capacity") +\
     geom_line(aes(y='K1'), color='blue') +\
     geom_line(aes(y='K2'), color='red') +\
@@ -89,21 +94,23 @@ def ddSim (y,t,r,K):
 # ex. define initial val (N), time steps, param(k)
 r=0.1
 K=[50]
-times=range(0,3)
-# list of r-values
+times=range(0,500)
+# list of N-values
 Ns=[1,50,100]
-# make Df to store results. Needs column for time and each r. 
+# make Df to store results. Needs column for time and each N. 
 Q1C=pandas.DataFrame({"times":times,"N1":0,"N2":0,"N3":0})
-# Write a for loop to evaluate model at each r. 
+# Write a for loop to evaluate model at each N. 
 for i in range(0,len(Ns)):
     params=(Ns[i],r)
     sim=spint.odeint(func=ddSim,y0=N0,t=times,args=params)
     Q1C.iloc[:,i]=sim[:,0]
-print ggplot(aes(x='times'), data=Q1C)+\
+    
+print ggplot(aes(x='times'), data=Q1C) +\
     xlab("time")+ylab("population size") +\
     geom_line(aes(y='N1'), color='blue') +\
     geom_line(aes(y='N2'), color='yellow') +\
     geom_line(aes(y='N3'), color='green') 
+
 
 
 if __name__ == '__main__':
