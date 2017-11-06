55887cb974f0b9b24c4654ce2bd1b7eb204c5c34
Om Neelay <oneelay@nd.edu>
2017-11-06 14:45:45 -0500

    Added starting file

---
+++ Exercise10.py
@@ -0,0 +1,14 @@
+#import packages
+import os
+import numpy as np
+import pandas as pd
+import scipy
+import sklearn
+import scipy.integrate as spint
+from plotnine import *
+import ggplot
+
+#set working directory
+os.chdir('/Users/omneelay/Desktop/')
+
+#
