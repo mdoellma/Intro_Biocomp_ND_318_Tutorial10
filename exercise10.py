85badf26ad3f4d7e60b56ac5a2af06b377c20423
Aaron Long <along10@nd.edu>
2017-11-07 01:02:55 -0500

    set up the simulations and did all the calculations for challenge 2

--- exercise10.py
+++ exercise10.py
@@ -83,26 +83,86 @@ modelDDSim([1, 5, 100], range(0,100), [0.2], [1, 50, 100])
 def epiSim(y,t,B,r):
     I=y[0]
     S=y[1]
+    R=y[2]
     
-    dSdt= (-B*I*S)
-    dIdt= (B*I*S)-(r*I)
-    dRdt= (r*I)
+    dSdt=(-B*I*S)
+    dIdt=(B*I*S)-(r*I)
+    dRdt=(r*I)
     
     return[dSdt,dIdt,dRdt]
 
 #put list of B and R parameters into dataframe
 paramB=[0.0005,0.005,0.0001,0.00005,0.0001,0.0002,0.0001]
 paramr=[0.05,0.5,0.1,0.1,0.05,0.05,0.06]
-param_array=numpy.column_stack([paramB,paramR])
+param_array=numpy.column_stack([paramB,paramr])
 param_df=pandas.DataFrame(param_array,columns=['B','r'])
 
 #extract params from dataframe
-for i in range(0,6):
-    I0=[1]
-    S0=[999]
+#dictionary variable
+d={}
+for i in range(0,7):
+    I0=1
+    S0=999
+    R0=0
+    y0=[I0,S0,R0]
     params=(param_df.B[i],param_df.r[i])
-    times=range(0,500)
-    #simulate model using odeint
-    modelSim=spint.odeint(func=epiSim,y0=(I0,S0),t=times,args=params)
+    t=range(0,501)
     
+    #simulate model using odeint and store in dictionary
+    modelSimISR=spint.odeint(epiSim,y0,t,params)
+    d["Sim{0}".format(i)]=pandas.DataFrame({'t':t,'I':modelSimISR[:,0],'S':modelSimISR[:,1],'R':modelSimISR[:,2]})
+
+#calculate maximum incidence, maximum prevalence, percent affected and basic reproduction number
+inclist=[]
+prevlist=[]
+percafflist=[]
+brnlist=[]
+A=list(d.keys())
+for i in range(0,7):
+    B=A[i]
+    df=d[B]
+    
+    C=[]
+    for x in range(0,501):
+        if x!=0:
+            N=x-1
+            Ip=df.iloc[N,0]
+            If=df.iloc[x,0]
+            incidence=If-Ip
+            C.append(incidence)
+    M=max(C)
+    inclist.append(M)
+    
+    D=[]
+    for y in range(0,501):
+        I=df.iloc[y,0]
+        S=df.iloc[y,2]
+        R=df.iloc[y,1]
+        prev=(I/(S+I+R))
+        D.append(prev)
+    M=max(D)
+    prevlist.append(M)
+    
+    I=df.iloc[500,0]
+    R=df.iloc[500,1]
+    S=df.iloc[500,2]
+    percaff=((I+R)/(S+I+R))
+    percafflist.append(percaff)
+    
+    I=df.iloc[0,0]
+    R=df.iloc[0,1]
+    S=df.iloc[0,2]
+    B=param_df.B[i]
+    r=param_df.r[i]
+    brn=((B*(S+I+R))/r)
+    brnlist.append(brn)
+    
+F=list(range(0,7,1))
+G=pandas.DataFrame(numpy.column_stack([F,inclist,prevlist,percafflist,brnlist]),columns=['Simulation','Max Prevalence','Percent Affected','Basic Reproduction #'])
+Results=
 ### --- End Challenge 2---------------------------------------
+print(percafflist)
+print(prevlist)
+print(brnlist)
+print(inclist)
+
