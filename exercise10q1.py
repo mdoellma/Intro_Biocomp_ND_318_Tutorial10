a990aa7a330da1696f0ec197c1efaa0b4418782a
Grant Keller <gkeller1@nd.edu>
2017-11-08 12:52:16 -0500

    Fixed issues with indentation, imports, redundant functions. Filled main function, added in module descriptors for part1, part2, part3.

--- exercise10q1.py
+++ exercise10q1.py
@@ -7,113 +7,98 @@ Authors: Grant Keller and Kathleen Nicholson
 
 
 import pandas
+from time import sleep
 from scipy.integrate import odeint as oi
 from plotnine import *
 
 def ddSim(y, t0, r, K):
     """
-    Function descriptor.
+    Population (N) over time function. Returns dN/dt.
     """
     return [r*y[0]*(1-y[0]/K)]
-
-# storage = pandas.DataFrame()
-# store_rs.iloc[:,i]=sim[:,0]
-# sim=si.odeint(func=ddSim,y0=N0[i],t=times,args=pars)
-# numpy.max(sim[:,1]/)
     
 def part1():
     """
+    Plots population size vs. time of 5 populations.
+    N0 = 10, K = 100.
+    r varies.
     """
-times = range(600)
-N0, K = 10, 100
-QIA = pandas.DataFrame({"times":times,"r1":0, "r2":0,"r3":0, "r4":0, "r5":0})
-rs = [-0.1, 0.1, 0.4, 0.8, 1.0]
-for i, r in enumerate(rs):
-    params = (r, K)
-    sim = spint.odeint(func=ddSim, y0=N0, t=times,args=params)
-    QIA.iloc[:, i] = sim[:, 0]
-print ggplot(aes(x='times'), data=QIA) +\
-    labs(title= "population size over time", x="time", y="population size") +\
-    geom_line(aes(y='r1'), color='blue') +\
-    geom_line(aes(y='r2'), color='red') +\
-    geom_line(aes(y='r3'), color='green') +\
-    geom_line(aes(y='r4'), color='yellow') +\
-    geom_line(aes(y='r5'), color='orange')
-
+    times = range(500)
+    N0, K = 10, 100
+    QIA = pandas.DataFrame({"times":times,
+                            "r1":0, "r2":0,
+                            "r3":0, "r4":0, "r5":0})
+    rs = [-0.1, 0.1, 0.4, 0.8, 1.0]
+    for i, r in enumerate(rs):
+        params = (r, K)
+        sim = oi(func=ddSim, y0=N0, t=times,args=params)
+        QIA.iloc[:, i] = sim[:, 0]
     
-    # plot data
+    print ggplot(aes(x='times'), data=QIA) +\
+        labs(title= "population size over time", x="time", y="population size") +\
+        geom_line(aes(y='r1'), color='blue') +\
+        geom_line(aes(y='r2'), color='red') +\
+        geom_line(aes(y='r3'), color='green') +\
+        geom_line(aes(y='r4'), color='yellow') +\
+        geom_line(aes(y='r5'), color='orange')
     
 def part2():
     """
-    • A plot of population size as a function of time with three 
-    populations (one line per population) possessing different 
-    carrying capacities (K = 10, 50, and 100). Use r = 0.2 and
-    N0 = 1 (these were somewhat arbitrarily chosen to make an 
-    informative figure).
+    Plots population size vs. time of 5 populations.
+    N0 = 1, r = 0.2.
+    K varies.
     """
-def ddSim (y,t,r,K):
-    N=y[0]
-    dNdt=r*(1-N/K)*N
-    return [dNdt]
-# define initial values (these don't change), state variables, time steps, params (that aren't changing)
-# ex. define initial val (N), time steps, param(k)
-r=0.2
-N0=1
-times=range(0,500)
-# list of K-values
-Ks=[10,50,100]
-# make Df to store results. Needs column for time and each K. 
-Q1B=pandas.DataFrame({"times":times,"K1":0,"K2":0,"K3":0})
-# Write a for loop to evaluate model at each K. 
-for i in range(0,len(Ks)):
-    params=(r,Ks[i])
-    sim=spint.odeint(func=ddSim,y0=N0,t=times,args=params)
-    Q1B.iloc[:,i]=sim[:,0]
-print ggplot(aes(x='times', color='K'), data=Q1B) +\
-    labs(title= "population size over time", x="time", y="population size") +\
-    geom_line(aes(y='K1'), color='blue') +\
-    geom_line(aes(y='K2'), color='red') +\
-    geom_line(aes(y='K3'), color='green')
-
-    
-
-
-
-
+    N0, r = 1, 0.2
+    times = range(500)
+    # list of K-values
+    Ks = [10, 50, 100]
+    # make Df to store results. Needs column for time and each K. 
+    Q1B = pandas.DataFrame({"times":times, "K1":0,
+                          "K2":0, "K3":0})
+    # Write a for loop to evaluate model at each K. 
+    for i, K in enumerate(Ks):
+        params = (r, K)
+        sim = oi(func=ddSim, y0=N0, t=times, args=params)
+        Q1B.iloc[:, i] = sim[:, 0]
+    print ggplot(aes(x='times', color='K'), data=Q1B) +\
+        labs(title= "population size over time", x="time", y="population size") +\
+        geom_line(aes(y='K1'), color='blue') +\
+        geom_line(aes(y='K2'), color='red') +\
+        geom_line(aes(y='K3'), color='green')
 
 def part3():
     """
-    • A plot of population size as a function of time with three
-    populations (one line per population) that shows the effect of
-    initial population size (N0 = 1, 50, and 100). Use a r = 0.1
-    and K = 50 for these simulations.
+    Plots population size vs. time of 5 populations.
+    K = 50, r = 0.1.
+    N0 varies.
     """
-def ddSim (y,t,r,K):
-    N=y[0]
-    dNdt=r*(1-N/K)*N
-    return [dNdt]
-# define initial values (these don't change), state variables, time steps, params (that aren't changing)
-# ex. define initial val (N), time steps, param(k)
-r=0.1
-K=[50]
-times=range(0,500)
-# list of N-values
-Ns=[1,50,100]
-# make Df to store results. Needs column for time and each N. 
-Q1C=pandas.DataFrame({"times":times,"N1":0,"N2":0,"N3":0})
-# Write a for loop to evaluate model at each N. 
-for i in range(0,len(Ns)):
-    params=(r,Ns[i])
-    sim=spint.odeint(func=ddSim,y0=N0,t=times,args=params)
-    Q1C.iloc[:,i]=sim[:,0]
-    
-print ggplot(aes(x='times'), data=Q1C) +\
-    labs(title= "population size over time", x="time", y="population size") +\
-    geom_line(aes(y='N1'), color='blue') +\
-    geom_line(aes(y='N2'), color='yellow') +\
-    geom_line(aes(y='N3'), color='green') 
-
-
+    # define initial values (these don't change), state variables, time steps, params (that aren't changing)
+    # ex. define initial val (N), time steps, param(k)
+    K, r = 50, 0.1
+    times = range(500)
+    # list of N-values
+    N0s=[1, 50, 100]
+    # make Df to store results. Needs column for time and each N. 
+    Q1C=pandas.DataFrame({"times":times, "N1":0,
+                          "N2":0, "N3":0})
+    # Write a for loop to evaluate model at each N. 
+    for i, N0 in enumerate(N0s):
+        params = (r, K)
+        sim = oi(func=ddSim, y0=N0, t=times, args=params)
+        Q1C.iloc[:, i] = sim[:, 0]
+        
+    print ggplot(aes(x='times'), data=Q1C) +\
+        labs(title= "population size over time", x="time", y="population size") +\
+        geom_line(aes(y='N1'), color='blue') +\
+        geom_line(aes(y='N2'), color='yellow') +\
+        geom_line(aes(y='N3'), color='green') 
 
 if __name__ == '__main__':
-    pass
+    print "Part 1 - N0 = 10; K = 50; r = -0.1, 0.1, 0.4, 0.8, 1.0: "
+    part1()
+    sleep(5)
+    print "Part 2 - N0 = 1; K = 10, 50, 100; r = 0.2: "
+    part2()
+    sleep(5)
+    print "Part 3 - N0 = 1, 50, 100; K = 50; r = 0.1: "
+    part3()
