7db0a1543f96a39c73a9bf421157b718c6adbbee
Melissa Stephens <mpullins@nd.edu>
2017-11-09 17:41:37 -0500

    wrote custom function for Q2. unpacked state variables and defined equations

--- Q2_Excercise10.py
+++ Q2_Excercise10.py
@@ -2,23 +2,33 @@
 #Authors Melissa Stephens and Janice Love
 
 #import packages
+import numpy
+import pandas
+from scipy.optimize import minimize
+from scipy.stats import norm
+from scipy.stats import chi2
+from plotnine import*
 
 #beta and gamma are my params, ds/dt, dI/dt, and dR/dt is my [dNdt]
+
 beta=(.0005,.005,.0001,.00005,.0001,.0002,.0001)
 gamma=(.05,0.5,0.1,0.1,0.05,0.06)
 In=1
 Res=0
 Sus=999
 times=range(0,600)
-N=1000    #state variable
+    #state variables. y is a list of state variables.(I,R,S) need to unpack these one at a time in the custom function
 R0=(B0*N)/gamma
 
-#Write a custom function that defines the model
+#Write a custom function that defines the model. Unpack state variables one at a time. Then one differential equation for each state variable
 def SIR (y,t,beta,gamma):
-	N=y[0]
-	dS/dt=-beta*In*Sus
-	dI/dt=(beta*In*Sus)-(gamma*In)
-	dR/dt=gamma*In
+	Sus=y[0]
+	In=y[1]
+	Res=y[2]
+	dSdt=-beta*In*Sus
+	dIdt=(beta*In*Sus)-(gamma*In)
+	dRdt=gamma*In
+	return [dSdt,dIdt,dRdt]
 
 #Make a dataframe to store results
 Q2=pandas.DataFrame({"time":time, "
