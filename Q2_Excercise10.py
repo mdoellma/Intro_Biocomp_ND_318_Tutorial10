9f8b69e6c9d78622aa9a20edbb0094c2a5cdf614
Melissa Stephens <mpullins@nd.edu>
2017-11-09 18:06:29 -0500

    working on Q2, but not finished. Created a data frame with our variables and started for loop. Won't be able to finish it tonight

--- Q2_Excercise10.py
+++ Q2_Excercise10.py
@@ -16,7 +16,8 @@ gamma=(.05,0.5,0.1,0.1,0.05,0.06)
 In=1
 Res=0
 Sus=999
-times=range(0,600)
+N0=[Sus,In,Res]
+times=range(0,500)
     #state variables. y is a list of state variables.(I,R,S) need to unpack these one at a time in the custom function
 R0=(B0*N)/gamma
 
@@ -30,12 +31,12 @@ def SIR (y,t,beta,gamma):
 	dRdt=gamma*In
 	return [dSdt,dIdt,dRdt]
 
-#Make a dataframe to store results
-Q2=pandas.DataFrame({"time":time, "
+#Make a dataframe with beta, gamma, max daily incidence, max daily prevalence, percent affected, reproduction number
+Q2=pandas.DataFrame({"beta":beta,"gamma":gamma,"Incidence":0,"prevalence":0,"percent_affected":0,"R0":beta*1000/gamma)
 
 for i in range(0,len(gamma)):
 	params=(beta[i],gamma[i])
-	modelSim=spint.odeint(func=SIR,y0=N0,t=times,args=params)
-	Q2.iloc[:,i]=modelSim[:,0]
+	modelSim=spint.odeint(func=SIR,y0=N0,t=times,args=params) ##Because N0 has three values, there will be three columns
+	Q2.percent_affected[i]= ##always going to be on row i. Because our dataframe has a different combination of columns
 
 ggplot(Q2,aes(x=
